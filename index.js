9b4b67f749a912e7ed0459bcd9532ad0af240933
YemiS <yemi.jaiyeola@btinternet.com>
2020-01-28 17:33:03 +0000

    use get

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
-app.use( (req, res, next) => {
-  console.log('HEY!');
-  next();
+app.get('/', (req, res) => {
+  res.send('HEY!')
 })
 app.listen(3000, () => console.log('Server running on port 3000'))
