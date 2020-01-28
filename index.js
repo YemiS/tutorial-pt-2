2deccdb014d66ab80f7c1dc57d892c0807248ea8
YemiS <yemi.jaiyeola@btinternet.com>
2020-01-28 23:17:20 +0000

    add messag

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
-var adsS3 = require('./functions/s3');
+let adsS3 = require('./functions/s3');
 
-app.use('/', adsS3.writeS3());
+const router = express.Router();
+
+router.use(require('./functions/s3'));
+app.use(router);
+
+//var router = express.Router();
+//router.route('/', adsS3.writeS3());
+
+//app.use('/', adsS3.writeS3());
 app.listen(3000, () => console.log('Server running on port 3000'))
