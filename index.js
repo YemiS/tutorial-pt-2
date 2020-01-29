79aa7fd9d3f331b3a7bad555527ab5fc18ae48aa
YemiS <yemi.jaiyeola@btinternet.com>
2020-01-29 15:23:18 +0000

    add mesg

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 const adsS3 = require('./functions/s3');
 
-app.get('/', adsS3.writeS3());
+app.get('/', () => { adsS3.writeS3(); });
 
 //var router = express.Router();
 //router.route('/', adsS3.writeS3());
