397cf18cc24f88c43676482975476e7aae986417
YemiS <yemi.jaiyeola@btinternet.com>
2020-01-29 13:53:47 +0000

    add mesg

--- functions/s3/index.js
+++ functions/s3/index.js
@@ -1,6 +1,6 @@
 const  S3 = require('aws-sdk/clients/s3');
 
-const writeS3 = async (event, context, callback) => {
+const writeS3 =  (event, context, callback) => {
   
   const s3 = new S3();
   const params = {
@@ -9,9 +9,12 @@ const writeS3 = async (event, context, callback) => {
  }
 
   try {
-        await s3.putObject(params).promise();
+         s3.putObject(params).promise().then((response) => {
+            console.log( `a log sent successfully`);
+
+         });
         //callback(null, `a log written successfully`);
-        console.log(null, `a log sent successfully`);
+       
     }
     catch (error) {
         //callback(error.message);
