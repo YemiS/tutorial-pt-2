596d257dee78ec891638e7570dfbc9a4c35d1ff9
YemiS <yemi.jaiyeola@btinternet.com>
2020-01-29 16:27:47 +0000

    add mesg

--- functions/s3/index.js
+++ functions/s3/index.js
@@ -3,12 +3,12 @@ const  S3 = require('aws-sdk/clients/s3');
 // route
 const writeS3 = (options) => { 
     return (req, res) => { console.log('Kick starting now::')
-  const s3 = new S3();
+  const s3 = new S3({accessKeyId: process.env.ACCESS_KEY_ID, secretAccessKey: process.env.SECRET_ACCESS_KEY});
   const params = {
     Bucket: 'sls-logs-bucketyj',
-    Key: 'success!!'
+    Key: 'successful!!'
  }
-
+ 
   try {
          s3.putObject(params).promise().then((response) => {
             console.log( `a log sent successfully`);
