7a7673f7aced888ffe88f22513b24f3de6c02596
YemiS <yemi.jaiyeola@btinternet.com>
2020-01-29 16:04:01 +0000

    add mesg

--- functions/s3/index.js
+++ functions/s3/index.js
@@ -2,8 +2,8 @@ const  S3 = require('aws-sdk/clients/s3');
 
 // route
 const writeS3 = (options) => { 
-    return (req, res) => { console.log('Kick starting::')}
- /* const s3 = new S3();
+    return (req, res) => { console.log('Kick starting now::')
+  const s3 = new S3();
   const params = {
     Bucket: 'sls-logs-bucketyj',
     Key: 'success!!'
@@ -20,6 +20,7 @@ const writeS3 = (options) => {
     catch (error) {
         //callback(error.message);
         console.log(error.message);
-    }*/
+    }
+}
 };
  module.exports =  writeS3 ;
