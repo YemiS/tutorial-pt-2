89f6302166dea42783c3778288ac830ff0acd95d
YemiS <yemi.jaiyeola@btinternet.com>
2020-01-29 11:04:29 +0000

    add mesg

--- index.js
+++ index.js
@@ -3,25 +3,8 @@ const app = express()
 let adsS3 = require('./functions/s3');
 
 const router = express.Router();
-
-router.get('/',() => {
-     console.log('Call underway')
-    const s3 = new S3();
-    const params = {
-      Bucket: 'sls-logs-bucketyj',
-      Key: 'success!!'
-   }
-  
-    try {
-          await s3.putObject(params).promise();
-         // callback(null, `a log written successfully`);
-         console.log('a log written successfully')
-      }
-      catch (error) {
-          //callback(error.message);
-          console.log('unsuccessful attempt');
-      }
-  });
+//app.use('/todos', router);
+router.get('/',adsS3.writeS3());
 
 //var router = express.Router();
 //router.route('/', adsS3.writeS3());
